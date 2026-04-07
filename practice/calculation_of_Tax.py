19afdbf2490b19e9b3235040303c4b19049da5f8
tatumlallak2026 <Tatumlallak2026@gmail.com>
2026-04-07 15:19:16 -0500

    calculating tax

---
+++ practice/calculation_of_Tax.py
@@ -0,0 +1,16 @@
+def item():
+    return input("enter item name: ")
+
+def applecost():
+    return .99
+
+def rate():
+    return 6.875
+
+def price():
+    return applecost()
+
+def total():
+    return rate() * price()
+
+print(total())
