e07f8b8b02d64f9da19382de8b17b3d9d65ae0e2
tatumlallak2026 <Tatumlallak2026@gmail.com>
2026-04-08 14:49:51 -0500

    calculator.py

---
+++ assignments/calculator.py
@@ -0,0 +1,15 @@
+x = input("enter 1st number: ")
+y = input("enter 2nd number: ")
+#reciving data from user
+opperation = input("enter operation")
+#asking for opperation
+if opperation == "+":
+    print (int(x)+int(y))
+elif opperation == "-":
+    print (int(x)-int(y))
+elif opperation == "*":
+    print (int(x)*int(y))
+elif opperation == "/":
+    print (int(x)/int(y))
+#opperation selection and calculation
+
