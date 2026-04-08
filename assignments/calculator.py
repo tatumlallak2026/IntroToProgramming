b3e79a918759e0db6bc1912692fe3d2fd005813d
tatumlallak2026 <Tatumlallak2026@gmail.com>
2026-04-08 15:06:43 -0500

    improved calc

--- assignments/calculator.py
+++ assignments/calculator.py
@@ -1,8 +1,17 @@
+
+
 x = input("enter 1st number: ")
+if x.isdigit() == False:
+        print ("invalid input") 
+
 y = input("enter 2nd number: ")
+if y.isdigit() == False:
+        print ("invalid input")
 #reciving data from user
-opperation = input("enter operation")
+
+opperation = input("enter operation: ")
 #asking for opperation
+
 if opperation == "+":
     print (int(x)+int(y))
 elif opperation == "-":
@@ -11,5 +20,14 @@ elif opperation == "*":
     print (int(x)*int(y))
 elif opperation == "/":
     print (int(x)/int(y))
+elif opperation == "^":
+    print (int(x)**int(y))
+elif opperation == "abs":
+    print (abs(int(x)))
+    print (abs(int(y)))
+elif opperation == "floor":
+     print (int(x)//int(y))
+elif opperation == "mod":
+    print (int(x)%int(y))
 #opperation selection and calculation
 
