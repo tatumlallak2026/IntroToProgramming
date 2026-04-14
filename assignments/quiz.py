733634d9a2e042fca9431e6d4e04d4ec587b8846
tatumlallak2026 <Tatumlallak2026@gmail.com>
2026-04-14 14:23:16 -0500

    QUIZ

---
+++ assignments/quiz.py
@@ -0,0 +1,12 @@
+answer = input("what is ram? ")
+if answer == "random access memory":
+    print("correct")
+else:
+    print("incorrect")
+
+answer = input("what is cpu? ")
+if answer == "central processing unit":
+    print("correct")
+else:
+    print("incorrect")
+    
